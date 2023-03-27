443f58936a3ed3bed9854ea32a1aed8d227981c6
Anton <antonoliverkiiski@gmail.com>
2023-03-27 10:45:43 +0300

    Create footer, map, places, places-detail and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlacesComponent } from './places/places.component';
+import { PlacesDetailComponent } from './places-detail/places-detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlacesComponent,
+    PlacesDetailComponent,
+    FooterComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/places-detail/places-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-places-detail',
+  templateUrl: './places-detail.component.html',
+  styleUrls: ['./places-detail.component.css']
+})
+export class PlacesDetailComponent {
+
+}
