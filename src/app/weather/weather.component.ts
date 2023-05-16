fa613ac233daf1d152ded20421dd76a9117b37f4
Suvikan <sanledell@gmail.com>
2023-05-16 09:40:56 +0300

    fix weather to use given coords

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { PlacesService } from '../places.service';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -9,25 +11,27 @@ import { faClock } from '@fortawesome/free-solid-svg-icons';
 export class WeatherComponent implements OnInit {
 
   city = 'Helsinki';
-  lat = 60.1695;
-  lon = 24.9355;
+  currentPosition!: { latitude: number, longitude: number };
   key = '24f3dc36e8779f81f566b7850c7e3f5e';
   units = 'metric';
   lang = 'fi';
-  url = `http://api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lon}&appid=${this.key}&units=${this.units}&lang=${this.lang}`;
+  //url = `http://api.openweathermap.org/data/2.5/forecast?lat=${this.currentPosition.latitude}&lon=${this.currentPosition.longitude}&appid=${this.key}&units=${this.units}&lang=${this.lang}`;
   weatherForcast: any = [];
   weather: any;
   day: any;
   faClock = faClock;
 
-  constructor(){}
+  constructor(private places: PlacesService){}
 
   ngOnInit(): void {
+    this.places.getCurrentPositionObservable().subscribe((position) => {
+      this.currentPosition = position;
     this.getWeatherForecast();
+  });
   }
 
   getWeatherForecast(){
-    fetch(this.url)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${this.currentPosition.latitude}&lon=${this.currentPosition.longitude}&appid=${this.key}&units=${this.units}&lang=${this.lang}`)
     .then(resp=>{
       if(!resp.ok) throw new Error(resp.statusText)
       return resp.json()
